57953da59e9e4a89e7266f994ad653f4d1f7ce29
sjm developer <226819177+gamingsjm42-bot@users.noreply.github.com>
2026-07-20 21:49:50 +0100

    Add files via upload

--- saveVars.cpp
+++ saveVars.cpp
@@ -5,21 +5,6 @@
 #ifndef defaultSavePath
 #define defaultSavePath ""
 #endif
-/// @brief that function transform to char* from const char*
-/// @param __i that const  char* will transform it
-char* toChar(const char* __i)
-{
-    char* returning;
-    for (size_t i = 0; i < sizeof(__i)/sizeof(__i); i++) returning[i] = __i[i];
-    return returning;
-}
-/// @brief that function to transform to const char* from char*
-/// @param __i that that char* will transform it
-const char* toChar(char* __i)
-{
-    const char* returning =__i;
-    return returning;
-}
 /// @brief that function add path default path
 /// @param path that path will add it
 std::string __addGlobalPath  (std::string path)
@@ -60,76 +45,79 @@ const char* load(const char* varName,bool defaultPathAdd= true)
     //returning
     return returningStr.c_str();
 }
-int strTo(char i){if(int('0')<=int(i)<=int('9')) int(i)-int('0');throw i;}
-char Tostr(int i){if( 0<i<9)return i+int('0');else throw i;}
-/// @brief that function return int from char
-/// @param __l that character transform it
-int strTo(const char* __l,int)
+/// @brief that funcion transform  from int to const char*
+/// @param number that function 
+const char* toStr(int number)
 {
-    //set base vars
-    int returning=0;
-    int beginLoop = 0;
-    //fix minus in math
-    if(__l[0]=='-') beginLoop=1; 
     //set returning var
-    for (size_t i =  beginLoop; i < sizeof(__l)/sizeof(const char); i++) returning+=strTo(__l[i])*pow(10,i);
-    //fix minus
-    if(beginLoop)returning*=-1; 
-    //return
-    return returning;
-}
-/// @brief that function transform from float to const char*
-/// @param __l that const char* will transfrom it
-float strTo(const char* __l,float)
-{
-    //set strVar
-    std::string str__L =__l;
-    //set Find Comma str
-    size_t commaFind=str__L.find(',');
-    //erase  ,
-    str__L.erase(',');
-    //returning
-    return  strTo(str__L.c_str(),int())/pow(10,commaFind);
-}
-    //set len fn
-auto len = [](int l)
+    std::string returning; 
+    //set int('l')to l
+    auto intToChar= [](int num) {return char(num-int('0'));};
+    //get len
+    auto len = [](int num){int returning ;while(abs(num)>1)returning/=10;return returning;};
+    //set last element fn
+    auto getLastElemnt = [&len](int num){num/=pow(10,len(num));return num;};
+    //fix-
+    if(number<0) returning+='-';
+    //set str
+    for (size_t i = 0; i < len(number); i++)
     {
-        //set var
-        size_t returning = 0;
-        while (l>=0)l/=10;returning++;
-        return returning;
-    };
-/// @brief that transform int to const char*
-/// @param l that integer to transform it
-const char* toStr(int l)
-{
-    //set begin
-    int begin = l>0;
-    //set returning var
-    char* returning;
-    //fix -
-    if(!begin)   returning[0]= '-';
-    //add returning char
-    for (size_t i =begin; i <len(l) ; i++) returning[i]=Tostr(int(l/len(l)));
-    //return
-    return returning;
-}
-/// @brief that transform from float to const char*
-/// @param l that float transform it
-const char* toStr(float l)
-{
-    //set retuning var
-    std::string returning;
-    //add l without comma
-    returning+=toStr(int(l));
-    //set l to not comma it
-    l-=int(l);
-    //set l int
-    while (l!=int(l)) l*=10;
-    //add comma
-    returning+=',';
-    //add l without comma
-    returning+=toStr(l); 
+        //add 'last' char
+        returning+= intToChar(getLastElemnt(number));
+        //refreshnum
+        number-= getLastElemnt(number)*(pow(10,len(number)));
+    }
     //return
     return returning.c_str();
+}
+/// @brief that function transform const char* from int
+/// @param number that number you transform it
+const char* toStr(float number)
+{
+    //set returning var
+    std::string returning =toStr(int(number))+',';
+    //remove integer side
+    number-=int(number);
+    //set number to integer after commas
+    for (; number==int(number);number*=10);
+    //add number after ,
+    returning +=toStr(int(number));
+    //return
+    return returning.c_str();
+}
+//types 
+namespace types
+{
+    double Double =0;
+    int Integer = 0;
+} // namespace types
+
+/// @brief that function transfrom to int from const char*
+/// @param number that will you transform it
+double strTo(const char* number,double)
+{
+    //set loopBegin var
+    int loopBegin = (number[0]=='-');
+    //set __strTo fn
+    auto __strTo = [](const char i){if(i==',') return 0;return int(i-int('0'));};
+    //set comma find
+    size_t findComma= 0;
+    for (size_t i = 0; i < sizeof(number)/sizeof(const char); i++) if(number[i]==',') findComma=i;
+    //set returning var
+    int returning=0;
+    //add returning char by char
+    for (size_t i = loopBegin; i < sizeof(number)/sizeof(const char); i++) returning+=pow(10,i-findComma)*(__strTo(number[i])) ;
+    //fix -
+    if(loopBegin) returning*=-1;
+    //return
+    return returning;
+    
+}
+/// @brief that const char* from ibt
+/// @param number that number will transform it
+/// @return 
+int strTo(const char* number,int)
+{
+    //return 
+    return strTo(number,types::Double);
 }
